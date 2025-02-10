7df8d9c0fcd7afe4e8fcb5519b58e384ab3c694d
AwaisSabir007 <awaissabir16@gmail.com>
2025-02-10 23:55:28 +0500

    updated

---
+++ src/mcq_gen/utils.py
@@ -0,0 +1,51 @@
+import os
+import PyPDF2
+import json
+import traceback
+
+def read_file(file):
+    if file.name.endswith(".pdf"):# if the data is in the form of pdf 
+        try:
+            pdf_reader=PyPDF2.PdfFileReader(file)
+            text=""
+            for page in pdf_reader.pages:
+                text+=page.extract_text()
+            return text
+            
+        except Exception as e:
+            raise Exception("error reading the PDF file")
+        
+    elif file.name.endswith(".txt"): # if the data is in the form of text file
+        return file.read().decode("utf-8")
+    
+    else:
+        raise Exception(
+            "unsupported file format only pdf and text file suppoted"
+            )
+
+def get_table_data(quiz_str): # this function is used to convert the quiz string to a table data
+    try:
+        # convert the quiz from a str to dict
+        quiz_dict=json.loads(quiz_str) # this is used to convert the string to dictionary
+        quiz_table_data=[]
+        
+        # iterate over the quiz dictionary and extract the required information
+        for key,value in quiz_dict.items():
+            mcq=value["mcq"]
+            options=" || ".join(
+                [
+                    f"{option}-> {option_value}" for option, option_value in value["options"].items()
+                 
+                 ]
+            )
+            
+            correct=value["correct"]
+            quiz_table_data.append({"MCQ": mcq,"Choices": options, "Correct": correct})
+        
+        return quiz_table_data
+        
+    except Exception as e:
+        traceback.print_exception(type(e), e, e.__traceback__)
+        return False
+
+
