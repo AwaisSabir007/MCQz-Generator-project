70b363f67c87e9ae741e2591d1141766902998e5
AwaisSabir007 <awaissabir16@gmail.com>
2025-02-10 18:13:45 +0500

    new update

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages,setup
 
 setup(
-    name="mcq_gen",
-    version="0.0.1",
-    author="Awais Sabir",
-    author_email="awaissabir16@gmail.com",
-    intall_requires=['openai', 'streamlit','langchain', 'python-dotenv', 'pyPDF2'],
-    packages=find_packages(),
+    name='mcq_gen',
+    version='0.0.1',
+    author='Awais Sabir',
+    author_email='awaissabir16@gamil.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
 )
---
+++ test.py
@@ -0,0 +1,2 @@
+from src.mcq_gen.logger import logging  
+logging.info("This is a test message")
